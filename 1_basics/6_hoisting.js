ca6a58e9b1bf1d4b5126efaec6bb2b8e808a9ef7
JINS-DE <rlawlstjd044@naver.com>
2024-06-27 09:26:00 +0900

    hositing

--- 1_basics/6_hoisting.js
+++ 1_basics/6_hoisting.js
@@ -1,3 +1,16 @@
 /* 
 Hoisting
 */
+// console.log(name)
+// var name = "따오따오"
+// console.log(name)
+
+// 호이스팅이란 : 모든 변수 선언문이 코드의 최상단으로 이동되는 것처럼 느껴지는 현상
+// 위에 의미와 같은 의미, hoisting
+// var name;
+// console.log(name);
+// var name = "따오따오"
+// console.log(name)
+
+console.log(yuJin);
+let yuJin = '안유진' ; //const, let 호이스팅이 발생한다. 
